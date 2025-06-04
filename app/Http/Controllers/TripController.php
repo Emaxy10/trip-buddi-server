8caf50a90210789ff11f968b12e74f2af99303b4
Emaxy10 <emmanueliroawula@outlook.com>
2025-06-04 01:30:24 +0100

    04/6/25

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -12,29 +12,46 @@ class TripController extends Controller
     //
    
     public function store(StoreTripRequest $request){
-        $trips = [];
+            $trips = [];
 
-
-            // Decode each passenger JSON string
-    $passengers = collect($request->input('passenger'))
-    ->map(fn($p) => json_decode($p, true));
+        $passengers = collect($request->input('passenger'))
+                        ->map(fn($p) => json_decode($p, true));
 
         $dest = $request->input('destination');
         $start_date = $request->input('start_date');
         $end_date = $request->input('end_date');
         $place_id = $request->input('place_id');
-        foreach ( $passengers as $passenger){
-           $trips[] = Trip::create([
-                'destination' => $dest,
-                'place_id' => $place_id,
-                'start_date' => $start_date,
-                'end_date' => $end_date,
-                'passenger' => $passenger['name']
-            ]);
+
+        foreach ($passengers as $passenger) {
+            // Check if trip already exists
+            $existingTrip = Trip::where('passenger', $passenger['name'])
+                ->where('place_id', $place_id)
+                ->where('start_date', $start_date)
+                ->where('end_date', $end_date)
+                ->first();
+
+            if (!$existingTrip) {
+                // Create new trip
+                $trips[] = Trip::create([
+                    'destination' => $dest,
+                    'place_id' => $place_id,
+                    'start_date' => $start_date,
+                    'end_date' => $end_date,
+                    'passenger' => $passenger['name'],
+
+                    
+                ]);
+
+                 return response()->json([
+                'trips' => $trips
+        ], 201);
+            } else {
+                 return response()->json([
+                    'error' => "Trip already exists for passenger "
+                ], );
+            }
         }
 
-        return response()->json([
-            'trips' => $trips
-        ], 201);
+       
     }
 }
